8349ded338a311c71890e49607631da36c87cfa1
Somnath Shinde <soma.shinde@gmail.com>
2021-08-11 11:52:46 +0530

    Working config server repository

---
+++ src/test/java/com/somshine/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somshine.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
